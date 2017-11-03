106eb2df4795558f0f5a73913be0ada090b6aac7
Gz <354300537@qq.com>
2017-11-03 21:44:26 +0800

    js开始第一步

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,25 @@ app.controller("ec",["$http","$scope",function($http,$scope){
 		$scope.nav = info.nav;
 //		var width = screenWidth*info.banner.length;
 //		$(".banner ul").eq(0).css({"width":width});
-		
 	})
 }])
+
+$(document).ready(function(){
+	
+	var screenScroll;
+	$(window).on("scroll",function(){
+		screenScroll = $(window).scrollTop();
+		changBg("#search",screenScroll,"#nav");
+	})
+})
+
+//滚动改变背景
+function changBg(ele,sS,ele2){
+	//ele 需要改变的元素  sS 屏幕卷的高度 ele2 目标元素距离
+	var top = $(ele2).offset().top;
+	var num = sS/top;
+	if(num>1){
+		num=1
+	}
+	$(ele).css("background","rgba(201, 21, 35,"+ num +")")
+}
